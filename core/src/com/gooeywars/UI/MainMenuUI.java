7d3288395efa04b266164c672e57d2f8edf66254
Matt Lajoie <LADJYUP@gmail.com>
2016-03-08 14:12:38 -0500

    UI now works somewhat. testing game stuff.

--- core/src/com/gooeywars/UI/MainMenuUI.java
+++ core/src/com/gooeywars/UI/MainMenuUI.java
@@ -9,8 +9,12 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.gooeywars.game.GooeyWars;
@@ -27,22 +31,50 @@ public class MainMenuUI implements Screen {
 	}
 	
 	public void create() {
-		stage = new Stage(new StretchViewport(200, 200));
 		batch = new SpriteBatch();
+		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
-		
+
+		// A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+		// recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
 		skin = new Skin();
-		
-		//Generating red skin
+		// Generate a 1x1 white texture and store it in the skin named "white".
 		Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
-		pixmap.setColor(Color.RED);
+		pixmap.setColor(Color.GREEN);
 		pixmap.fill();
+
+		skin.add("white", new Texture(pixmap));
+
+		// Store the default libgdx font under the name "default".
+		BitmapFont bfont=new BitmapFont();
+		skin.add("default",bfont);
+
+		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+
+		textButtonStyle.font = skin.getFont("default");
+
+		skin.add("default", textButtonStyle);
+
+		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+		final TextButton textButton=new TextButton("PLAY",textButtonStyle);
+		textButton.setPosition(200, 200);
+		stage.addActor(textButton);
+		stage.addActor(textButton);
+		stage.addActor(textButton);
 		
-		skin.add("red", new Texture(pixmap));
-		
-		//Default font storage
-		BitmapFont defaultFont = new BitmapFont();
-		skin.add("default", defaultFont);
+		textButton.addListener(new ChangeListener() {
+			
+			public void changed (ChangeEvent event, Actor actor) {
+				//System.out.println("Clicked! Is checked: " + button.isChecked());
+				textButton.setText("Starting new game");
+				GooeyWars.setCurrentBox(GooeyWars.getGame());
+			}
+		});
 	}
 	
 	@Override
