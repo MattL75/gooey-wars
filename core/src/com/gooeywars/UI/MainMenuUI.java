83892fadf53e17e8bb5530467a8377812748260a
Matt Lajoie <LADJYUP@gmail.com>
2016-03-08 13:40:27 -0500

    UI changes and stuff.

--- core/src/com/gooeywars/UI/MainMenuUI.java
+++ core/src/com/gooeywars/UI/MainMenuUI.java
@@ -1,7 +1,86 @@
 package com.gooeywars.UI;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class MainMenuUI extends Stage {
+public class MainMenuUI implements Screen {
+
+	Stage stage;
+	Skin skin;
+	SpriteBatch batch;
+	
+	public MainMenuUI() {
+		create();
+	}
+	
+	public void create() {
+		stage = new Stage(new StretchViewport(200, 200));
+		batch = new SpriteBatch();
+		Gdx.input.setInputProcessor(stage);
+		
+		skin = new Skin();
+		
+		//Generating red skin
+		Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
+		pixmap.setColor(Color.RED);
+		pixmap.fill();
+		
+		skin.add("red", new Texture(pixmap));
+		
+		//Default font storage
+		BitmapFont defaultFont = new BitmapFont();
+		skin.add("default", defaultFont);
+	}
+	
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height);
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
