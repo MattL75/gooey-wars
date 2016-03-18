b2d22feb6cd30cf50d7c108db33f1f42b6f69635
Karl Godin <godinkarl@gmail.com>
2016-03-18 13:43:32 -0400

    Cleaning up entity class

--- core/src/com/gooeywars/game/GooeyWars.java
+++ core/src/com/gooeywars/game/GooeyWars.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.gooeywars.UI.GameUI;
@@ -28,6 +29,7 @@ public class GooeyWars extends Main{
 	
 	@Override
 	public void create() {
+		System.out.println((new Vector2(20,23)));
 		DisplayMode displayMode = Gdx.graphics.getDisplayMode();
 
 		initializeScreen();
