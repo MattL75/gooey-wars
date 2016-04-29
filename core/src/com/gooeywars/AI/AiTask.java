b99f41c3f0540aee9856e7b095cee8f3f171691e
Matt Lajoie <LADJYUP@gmail.com>
2016-04-29 11:39:25 -0400

    Matt's fuck up

--- core/src/com/gooeywars/AI/AiTask.java
+++ core/src/com/gooeywars/AI/AiTask.java
@@ -1,19 +1,6 @@
 package com.gooeywars.AI;
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
-import com.gooeywars.entities.Goo;
-
 public class AiTask {
-	
-	public AiTask(){
-		
-	}
-	
-	public void move(Array<Goo> goos, Vector2 position){
-		
-	}
-	
 	public void execute(){
 		
 	}
