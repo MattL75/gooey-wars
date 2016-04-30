eb3a61d0d5cad255ff9373fb2e19ce6ee3dc100e
Matt Lajoie <LADJYUP@gmail.com>
2016-04-30 11:59:37 -0400

    Minimap fully implemented... Gamebox size currently 1920 * 2, 1080 * 2

--- core/src/com/gooeywars/game/GameBox.java
+++ core/src/com/gooeywars/game/GameBox.java
@@ -70,8 +70,8 @@ public class GameBox {
 		
 		size = new Vector2();
 		
-		size.x = 10000;
-		size.y = 10000;
+		size.x = 1920 * 2;
+		size.y = 1080 * 2;
 		
 		if(physicsEnabled){
 			physics = new PhysicsBox();
